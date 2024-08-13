e8befb150b7e163f686bcaad3450e03d6f494ced
Yersultan <nusupoversultan228@gmail.com>
2024-08-13 23:24:37 +0600

    update: moved images to package

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import requests
 import json
-import datetime 
+import datetime
 import time
 
 bot = telebot.TeleBot('7126095826:AAHg-r2KLV9D7IjurCy9LmmsaYWmlzfTbuo')
@@ -23,12 +23,20 @@ def getWeather(message):
         weatherInfo = data['weather'][0]
         current_hour = datetime.datetime.now().hour
         bot.reply_to(message, f'The current temperature outside is: {data["main"]["temp"]}')
-        image = 'img.png' if weatherInfo['description'] == 'clear sky' and 6 <= current_hour < 19 else 'img_9.png' if weatherInfo['description'] == 'clear sky' and 19 <= current_hour < 6 else 'img_1.png' if weatherInfo['description'] == 'few clouds' and 6 <= current_hour < 19 else 'img_10.png' if weatherInfo['description'] == 'few clouds' and 19 <= current_hour < 6 else 'img_2.png' if weatherInfo['description'] == 'scattered clouds' else 'img_3.png' if weatherInfo['description'] == 'broken clouds' else 'img_4.png' if 300 <= weatherInfo['id'] <= 321 else 'img_5.png' if weatherInfo['description'] == 'rain' and 6 <= current_hour < 19 else 'img_11.png' if weatherInfo['description'] == 'rain' and 19 <= current_hour < 6 else 'img_6.png' if 200 <= weatherInfo['id'] <= 232 else 'img_7.png' if 600 <= weatherInfo['id'] <= 622  else 'img_8.png'
+        image = 'images/img.png' if weatherInfo['description'] == 'clear sky' and 6 <= current_hour < 19 else 'images/images/img_9.png' if \
+        weatherInfo['description'] == 'clear sky' and 19 <= current_hour < 6 else 'images/img_1.png' if weatherInfo[
+                                                                                                     'description'] == 'few clouds' and 6 <= current_hour < 19 else 'images/img_10.png' if \
+        weatherInfo['description'] == 'few clouds' and 19 <= current_hour < 6 else 'images/img_2.png' if weatherInfo[
+                                                                                                      'description'] == 'scattered clouds' else 'images/img_3.png' if \
+        weatherInfo['description'] == 'broken clouds' else 'images/img_4.png' if 300 <= weatherInfo[
+            'id'] <= 321 else 'images/img_5.png' if weatherInfo[
+                                                 'description'] == 'rain' and 6 <= current_hour < 19 else 'images/img_11.png' if \
+        weatherInfo['description'] == 'rain' and 19 <= current_hour < 6 else 'images/images/img_6.png' if 200 <= weatherInfo[
+            'id'] <= 232 else 'images/img_7.png' if 600 <= weatherInfo['id'] <= 622 else 'images/img_8.png'
         file = open('./' + image, 'rb')
         bot.send_message(message.chat.id, f"The weather is {weatherInfo['description']}")
         bot.send_photo(message.chat.id, file)
     else:
         bot.reply_to(message, f'You entered an incorrect name of location!')
-
 
 bot.polling(non_stop=True)
